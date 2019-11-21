a97e785a5cfc5d4342993d9a6f264f8f49abea44
Ohtuna <30372986+ohtuna@users.noreply.github.com>
2019-11-21 15:06:49 +0900

    785A - Anton and Polyhedrons

---
+++ Codeforces/785A.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    string str;
+    int sum = 0;
+    for(int i=0;i<n; i++)
+    {
+        cin >> str;
+
+        if(str[0] == 'T')
+        {
+            sum +=4;
+        }
+        else if(str[0] == 'C')
+        {
+            sum +=6;
+        }
+        else if(str[0] == 'O')
+        {
+            sum += 8;
+        }
+        else if(str[0] == 'D')
+        {
+            sum +=12;
+        }
+        else
+        {
+            sum += 20;
+        }
+        
+    }
+
+    cout << sum;
+}

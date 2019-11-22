fc0a6d7ff84ec3a051eba143ea52e79bf5074d6b
Ohtuna <30372986+ohtuna@users.noreply.github.com>
2019-11-22 12:21:08 +0900

    959A - Mahmoud and Ehab and the even-odd game

---
+++ Codeforces/959A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n%2 == 1)
+    {
+        cout << "Ehab";
+    }
+    else
+    {
+        cout << "Mahmoud";
+    }
+    
+}

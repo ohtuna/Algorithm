983e7b79b81184a49c78e602fddd24c7140070e9
Ohtuna <30372986+ohtuna@users.noreply.github.com>
2019-11-27 04:07:02 +0900

    17256. 달달함이 넘쳐흘러

---
+++ BOJ/17256.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    int d, e, f;
+
+    cin >> a >> b >> c >> d >> e >> f;
+
+    cout << d-c<< " " << e/b << " " << f-a;
+
+}

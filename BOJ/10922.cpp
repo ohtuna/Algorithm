dd7649fdce2bb950acb98ab8c92150adee387ecb
Ohtuna <30372986+ohtuna@users.noreply.github.com>
2020-01-21 17:17:14 +0900

    10922. 별 찍기 - 17

---
+++ BOJ/10922.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int left = n-1;
+    int cnt = 0;
+    if(n==1)
+    {
+        cout << "*";
+        return 0;
+    }
+    for(int i =0; i<n; i++)
+    {
+        for(int j =0; j<n*2-1; j++)
+        {
+            if(j == left)
+            {
+                cout << "*";
+                left--;
+                if(i==0)break;
+                for(int k = left+1; k<n+cnt-1; k++)
+                {
+                    if(i <= n-2)
+                    {
+                        cout << " ";
+                    }
+                    if(i == n-1)
+                    {
+                        cout << "*";
+                    }
+                }
+               
+                cout << "*";
+                cnt++;
+                break;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        if(i <= n-2)
+        {
+            cout << "\n";
+        }
+    }
+}
